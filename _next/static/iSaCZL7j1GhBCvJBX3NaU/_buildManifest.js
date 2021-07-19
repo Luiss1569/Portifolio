9c7ef93b4c8fe50b3ededed230a1a65855e08032
Luis Ricardo Santos <rcsrcs1569@gmail.com>
2021-07-19 00:03:06 -0300

    Updates

---
+++ _next/static/iSaCZL7j1GhBCvJBX3NaU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/778-cc90d0112f1eaec2a5c1.js","static/chunks/pages/index-f371dcd805bd2541e914.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
